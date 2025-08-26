c991a5d896c550b3cda30836d02920c33e584296
Davi Queiroz <daviqueirozsc@gmail.com>
2025-08-26 08:52:15 -0300

    Alterações do meu revisor

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ car_data = pd.read_csv('vehicles_us.csv')
 
 st.header('Análise Exploratória de Dados (EDA) dos Carros')
 
-hist_button = st.button('Criar histograma de Distribuição de Combustível dos Carros')
+hist_button = st.button('Criar histograma de Distribuição de Preço dos Carros')
         
 if hist_button: 
-    st.write('Criando um histograma de Distribuição de Combustível dos Carros')
-    fig = px.histogram(car_data, x="fuel", title="Distribuição de Combustível dos Carros")
+    st.write('Criando um histograma de Distribuição de Preço dos Carros:')
+    fig = px.histogram(car_data, x="price", title="Distribuição de Preço dos Carros")
     st.plotly_chart(fig, use_container_width=True)
 
-hist_button2 = st.button('Criar histograma de Distribuição de Cores dos Carros')
+hist_button2 = st.button('Criar histograma de Distribuição de Cilindradas dos Carros')
 
 if hist_button2:
-    st.write('Criando histograma de Distribuição de Cores dos Carros')
-    fig = px.histogram(car_data, x="paint_color", title="Distribuição de Cores dos Carros")
+    st.write('Criando histograma de Cilindradas dos Carros:')
+    fig = px.histogram(car_data, x="cylinders", title="Distribuição de Cilindradas dos Carros")
     st.plotly_chart(fig, use_container_width=True)
 
-scatter_button = st.button('Criar gráfico de Dispersão entre Quilometragem e Preço dos Carros')
+scatter_button = st.button('Criar gráfico de Dispersão entre Quilometragem e Preço dos Carros:')
 
 if scatter_button:
     st.write('Criando gráfico de Dispersão entre Quilometragem e Preço dos Carros')
